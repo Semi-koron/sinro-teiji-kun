f2b08e62b65f51eec5b0680ac8a9464004d19c53
tbs-kun-period <e283.tsubasa@gmail.com>
2025-11-03 19:20:48 +0900

    signup

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,4 +1,42 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
-};
+  
+  return (
+  <>
+    <h1>〇〇〇にログインする</h1>
+    <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { m: 1, width: '50ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="メールアドレス"
+          placeholder="info@example.com"
+          defaultValue=""
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="パスワード"
+          placeholder="半角英数字"
+          defaultValue=""
+        />
+      </div>
+    </Box>
+    <Stack spacing={2} direction="row" justifyContent="center">
+      <Button variant="contained">ログイン</Button>
+    </Stack>
+  </>
+  );
+}
 export default LoginPage;
